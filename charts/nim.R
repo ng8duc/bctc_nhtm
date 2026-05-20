f699ba1b04f607eddbd24b8b2ef35cc5394970ff
Duc <nghdworks@gmail.com>
2026-05-20 12:47:18 +0700

    feat: change xAxis to datetime and format labels in nim chart

--- charts/nim.R
+++ charts/nim.R
@@ -16,7 +16,7 @@ chart_nim <- highchart() %>%
   hc_colors(colors = c("#006b68", "#fdb71a")) %>%
   hc_add_series(
     data = df,
-    mapping = hcaes(x = tick_labels, y = value * 100, group = chi_tieu),
+    mapping = hcaes(x = yq, y = value * 100, group = chi_tieu),
     type = "line",
     tooltip = list(
       valueSuffix = "%"
@@ -24,9 +24,17 @@ chart_nim <- highchart() %>%
     marker = list(enabled = TRUE, radius = 4)
   ) %>%
   hc_xAxis(
+    type = "datetime",
     gridLineWidth = 1,
     gridLineColor = "#e6e6e6",
-    categories = df$tick_labels
+    labels = list(
+      formatter = JS("function() {
+        var date = new Date(this.value);
+        var month = date.getUTCMonth() + 1;
+        var quarter = Math.ceil(month / 3);
+        return (quarter * 3) + 'T_' + date.getUTCFullYear();
+      }")
+    )
   ) %>%
   hc_yAxis(
     title = list(text = "%"),
@@ -35,7 +43,20 @@ chart_nim <- highchart() %>%
   hc_tooltip(
     shared = TRUE,
     crosshairs = TRUE,
-    valueDecimals = 2
+    valueDecimals = 2,
+    formatter = JS("function(tooltip) {
+      var date = new Date(this.x);
+      var month = date.getUTCMonth() + 1;
+      var quarter = Math.ceil(month / 3);
+      var xLabel = (quarter * 3) + 'T_' + date.getUTCFullYear();
+      
+      var s = '<b>' + xLabel + '</b><br/>';
+      this.points.forEach(function(point) {
+        s += '<span style=\"color:' + point.color + '\">●</span> ' + 
+             point.series.name + ': <b>' + Highcharts.numberFormat(point.y, 2) + '%</b><br/>';
+      });
+      return s;
+    }")
   ) %>%
   hc_add_theme(hc_theme_smpl()) %>%
   hc_legend(
