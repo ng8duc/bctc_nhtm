d39c72f83f6ea4b71a4606f9a73d93fd23ae1a1b
Duc <nghdworks@gmail.com>
2026-08-05 15:03:37 +0700

    Rename

--- _targets.R
+++ _targets.R
@@ -33,7 +33,7 @@ list(
   # 3. Target tự động render dashboard khi dữ liệu (all_tables) thay đổi
   tar_quarto(
     dashboard_report,
-    "report.qmd",
+    "index.qmd",
     quiet = FALSE
   )
 )
